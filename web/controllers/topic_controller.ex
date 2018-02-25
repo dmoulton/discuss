050dc612a975dc26c6caf5366b06d3101f1778b1
David Moulton <david.moulton@dish.com>
2018-02-24 22:38:26 -0700

    done with auth, created comments

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,10 +3,19 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:update, :edit, :delete]
+
   def index(conn, _params) do
     topics = Repo.all(Topic)
 
     render conn, "index.html", topics: topics
+  end
+
+  def show(conn, %{"id" => topic_id}) do
+    topic = Repo.get!(Topic, topic_id)
+
+    render conn, "show.html", topic: topic
   end
 
   def new(conn, _params) do
@@ -16,16 +25,18 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-     changeset = Topic.changeset(%Topic{}, topic)
+    changeset = conn.assigns.user
+      |> build_assoc(:topics)
+      |> Topic.changeset(topic)
 
-     case Repo.insert(changeset) do
-        {:ok, _topic} -> 
-          conn
-          |> put_flash(:info, "Topic Created")
-          |> redirect(to: topic_path(conn, :index))
-        {:error, changeset} -> 
-          render conn, "new.html", changeset: changeset
-     end
+    case Repo.insert(changeset) do
+      {:ok, _topic} -> 
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} -> 
+        render conn, "new.html", changeset: changeset
+    end
   end
 
   def edit(conn, %{"id" => topic_id}) do
@@ -55,5 +66,18 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You can't edit that")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
   end
 end
